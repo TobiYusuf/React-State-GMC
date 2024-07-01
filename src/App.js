9edee2dba555cdc5a3845d3e8b925d09a2b8992b
TobiYusuf <yoluwatobi05@gmail.com>
2024-07-01 21:16:07 +0100

    React state project commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "Tobi Yusuf",
+        bio: "Software Developer at GoMyCode.",
+        imgSrc:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPa99MCk4QM3Nb5Zlpjx21klJq1r_CMr7CNg&s",
+        profession: "Developer",
+      },
+      shows: false,
+      mountedTime: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ mountedTime: this.state.mountedTime + 1 });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow = () => {
+    this.setState({ shows: !this.state.shows });
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+
+    const { person, shows, mountedTime } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+        {shows && (
+          <div>
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <img src={imgSrc} alt="Profile" />
+            <p>Profession: {profession}</p>
+          </div>
+        )}
+        <p>Time since mount: {mountedTime} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
